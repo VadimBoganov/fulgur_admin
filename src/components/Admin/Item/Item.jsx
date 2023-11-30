6f58ba5396fc63e105c66cbf8f826828b4471bb5
vadimboganov <boganov.v@tn.ru>
2023-11-30 22:27:35 +0300

    add groups

--- src/components/Admin/Item/Item.jsx
+++ src/components/Admin/Item/Item.jsx
@@ -40,13 +40,14 @@ const Item = () => {
   const productItems = useSelector(({ productitems }) => productitems);
   const items = useSelector(({ items }) => items);
 
-  const groups = items.list.reduce((item, {Id, ProductItemId, Name, Price, IsFullPrice}) => {
-    if(!item[ProductItemId]) item[ProductItemId] = [];
-    item[ProductItemId].push({Id, Name, Price, IsFullPrice})
-    return item
-  }, {})
-
-  //Object.entries(groups).forEach((group) =>group[1].map(({Id, ProductItemId, Name, Price, IsFullPrice}, index) => console.log(index)))
+  const groups = items.list.reduce(
+    (item, { Id, ProductItemId, Name, Price, IsFullPrice }) => {
+      if (!item[ProductItemId]) item[ProductItemId] = [];
+      item[ProductItemId].push({ Id, ProductItemId, Name, Price, IsFullPrice });
+      return item;
+    },
+    {}
+  );
 
   return (
     <div className={styles.admin}>
@@ -61,58 +62,87 @@ const Item = () => {
           <button className={styles.add_button}>Добавить</button>
         </NavLink>
         <Accordion data-bs-theme="dark">
-          {groups && 
-            items.list.map(({ Id, ProductItemId, Name, Price, IsFullPrice }, index) => (
-              <Accordion.Item key={Id} eventKey={Id}>
-                <Accordion.Header>{Name}</Accordion.Header>
-                <Accordion.Body>
-                  <form key={Id} className={styles.form}>
-                    <FileInput
-                      item={items.list[index]}
-                      labelValue={"Изображение"}
-                      setValue={setFile}
-                    />
-                    <DropdownInput
-                      id={Id}
-                      value={ProductItemId}
-                      labelValue={"Название продукта"}
-                      setValue={setSelectValue}
-                      options={productItems.list}
-                    />
-                    <StringInput
-                      id={Id}
-                      value={Name}
-                      labelValue={"Название изделия"}
-                      setValue={setName}
-                    />
-                    <div className={styles.price}>
-                      <NumberInput
-                        id={Id}
-                        value={Price}
-                        labelValue={"Цена"}
-                        setValue={setPrice}
-                      />
-                      <CheckboxInput
-                        id={Id}
-                        value={IsFullPrice}
-                        labelValue={"Выводить цену (от)"}
-                        setValue={setIsFullPrice}
-                      />
-                    </div>
-                    <section>
-                      <UpdateButton data={{Id: Id,
-                              ProductItemId: selectValue ? productItems.list.filter((pi) => pi.Name === selectValue)[0]?.Id : ProductItemId,
-                              Name: name || Name,
-                              Price: price || Price,
-                              IsFullPrice: isFullPrice === undefined ? IsFullPrice : isFullPrice,
-                              File: file,
-                            }} func={updateItem}/>
-                      <RemoveButton id={Id} func={removeItem} />
-                    </section>
-                  </form>
-                </Accordion.Body>
-              </Accordion.Item>
-            ))}
+          {groups &&
+            Object.entries(groups).map((group, index) => {
+              return (
+                <div key={index}>
+                  <h3 key={index} className={styles.group}>
+                    {
+                      productItems.list.filter(
+                        (pi) => pi.Id === parseInt(group[0])
+                      )[0]?.Name
+                    }
+                  </h3>
+                  {group[1].map(
+                    (
+                      { Id, ProductItemId, Name, Price, IsFullPrice },
+                      index
+                    ) => (
+                      <Accordion.Item key={Id} eventKey={Id}>
+                        <Accordion.Header>{Name}</Accordion.Header>
+                        <Accordion.Body>
+                          <form key={Id} className={styles.form}>
+                            <FileInput
+                              item={items.list[index]}
+                              labelValue={"Изображение"}
+                              setValue={setFile}
+                            />
+                            <DropdownInput
+                              id={Id}
+                              value={ProductItemId}
+                              labelValue={"Название продукта"}
+                              setValue={setSelectValue}
+                              options={productItems.list}
+                            />
+                            <StringInput
+                              id={Id}
+                              value={Name}
+                              labelValue={"Название изделия"}
+                              setValue={setName}
+                            />
+                            <div className={styles.price}>
+                              <NumberInput
+                                id={Id}
+                                value={Price}
+                                labelValue={"Цена"}
+                                setValue={setPrice}
+                              />
+                              <CheckboxInput
+                                id={Id}
+                                value={IsFullPrice}
+                                labelValue={"Выводить цену (от)"}
+                                setValue={setIsFullPrice}
+                              />
+                            </div>
+                            <section>
+                              <UpdateButton
+                                data={{
+                                  Id: Id,
+                                  ProductItemId: selectValue
+                                    ? productItems.list.filter(
+                                        (pi) => pi.Name === selectValue
+                                      )[0]?.Id
+                                    : ProductItemId,
+                                  Name: name || Name,
+                                  Price: price || Price,
+                                  IsFullPrice:
+                                    isFullPrice === undefined
+                                      ? IsFullPrice
+                                      : isFullPrice,
+                                  File: file,
+                                }}
+                                func={updateItem}
+                              />
+                              <RemoveButton id={Id} func={removeItem} />
+                            </section>
+                          </form>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    )
+                  )}
+                </div>
+              );
+            })}
         </Accordion>
       </div>
     </div>
